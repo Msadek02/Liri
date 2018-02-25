2967ed992d609e348e3ebe1b559e7b132fcb1b66
Msadek02 <Thunder_axe_90@hotmail.com>
2018-02-25 14:35:14 -0500

    m

--- liri.js
+++ liri.js
@@ -1,56 +1,80 @@
 require("dotenv").config();
 
-// // var movieName = process.env[2];
-// // // ...
+var action = process.argv[2];
+// var value = process.argv[3]; (THIS ONLY TAKES IN ONE NODE ARGUMENT FOR THE 3RD NODE SO ADDED CODE BELOW)
+
+// STORE ALL OF THE ARGUMENTS IN AN ARRAY
+var nodeArgs = process.argv;
+
+// CREATE AN EMPTY VARIABLE FOR HOLDING USERS INPUT VALUE
+var value = "";
+
+// LOOPS THROUGH ALL THE WORDS IN THE NODE ARGUMENT 
+// DO A LOOP TO GET EVERYTHING AFTER THE INDEX OF 2 NODE ARGUMENT
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+
+        value = value + "+" + nodeArgs[i];
+
+    } else {
+
+        value = value + nodeArgs[i];
+    }
+}
 
 
-// // // Then run a request to the OMDB API with the movie specified
-// // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// // request(queryUrl, function (error, response , body){
-// // if (!error && response.statusCode === 200) {
-// // console.log("the movie was released in:",(JSON.parse(body)));
-// // }
-// // });
+switch (action) {
+  case 'my-tweets':
+      twitter();
+}
 
-// // var request = require('request');
-// // request('http://www.google.com', function (error, response, body) {
-// //     console.log('body:', body); // Print the HTML for the Google homepage.
-// // });
-// var Twitter = require('twitter');
+// var request = require("request");
 
-// var client = new Twitter(keys.twitter);
 
-// client.get('favorites/list', function(error, tweets, response) {
-//     if(error) throw error;
-//     console.log(tweets);  
-//     console.log(response);   
-//   });
+// var movieName = process.env[2];
+// // ...
+
+
+// // Then run a request to the OMDB API with the movie specified
+// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+// request(queryUrl, function (error, response , body){
+// if (!error && response.statusCode === 200) {
+// console.log("the movie was released in:",(JSON.parse(body)));
+// }
+// });
+function twitter() {
+
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+ 
+var client = new Twitter(keys.twitter);
+
+var status = 'statuses/user_timeline';
+var params = { screen_name: value, count: 20 };
+client.get(status, params, function(error, tweets, response) {
+  if(!error){
+    console.log("Here are the most recent tweets");
+
+    for (var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+    } 
+  }
+});
+}
+// var keys = require('./keys.js');
 
 // var Spotify = require('node-spotify-api');
  
-// Spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
+// var spotify = new Spotify(keys.spotify);
+
+// // var query = process.argv[3]
+
+// spotify
+//   .search({ type: 'track', query: "love" })
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
 //   });
-
-
-var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: 'mTtuna6SyS27E4no3D6X3yhbq',
-  consumer_secret: 'LLbo8NIJ3a8k8mAbPZHYc7nbPqUb5MUJlG3aRdviUHzB46rwEU',
-  access_token_key: '960692160898256896-i3iUYGq4Srzs84AA39h4y6fCcn1ioGb',
-  access_token_secret: 'Lcok0fYob4bVv3v2faftUB93KezbKmNmPAsSDYieevMUk'
-});
-
-var status = 'statuses/user_timeline';
- 
-var params = {screen_name: 'nodejs'};
-client.get(status , function(error, tweets, response) {
-  if(error) throw error;
-
-  for (var i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
-  }
-});
