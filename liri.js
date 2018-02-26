516c04ae6cca5c0f73e66c0fc68e9fe3b1602303
Msadek02 <Thunder_axe_90@hotmail.com>
2018-02-26 18:27:41 -0500

    omdb constructed function

--- liri.js
+++ liri.js
@@ -27,22 +27,34 @@ for (var i = 3; i < nodeArgs.length; i++) {
 switch (action) {
   case 'my-tweets':
       twitter();
+
+  case 'movie-this':
+      omdb();
 }
 
-// var request = require("request");
+
+function omdb(){
+   var request = require("request");
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
+request(queryUrl, function (error, response , body){
+if (!error && response.statusCode === 200) {
+// console.log("the name of this movie is:",(JSON.parse(body)));
+console.log("the name of this movie is:",(JSON.parse(body).Title));
+console.log("the movie was released in:",(JSON.parse(body).Year));
+console.log("IMDB Rating:",(JSON.parse(body).imdbRating));
+console.log("Rotten tomatoes Rating:",(JSON.parse(body).Ratings[1].Value));
+console.log("this movie was made in:",(JSON.parse(body).Country));
+console.log("the language of this movie is:",(JSON.parse(body).Language));
+console.log("the plot of this movie is:",(JSON.parse(body).Plot));
+console.log("Actors:",(JSON.parse(body).Actors));
+}
+});
+}
 
 
-// var movieName = process.env[2];
-// // ...
 
-
-// // Then run a request to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-// request(queryUrl, function (error, response , body){
-// if (!error && response.statusCode === 200) {
-// console.log("the movie was released in:",(JSON.parse(body)));
-// }
-// });
 function twitter() {
 
 var Twitter = require('twitter');
